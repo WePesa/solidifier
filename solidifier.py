fe7fb5ee61b8978ffc39459f4cdcb908d44651ba
Leonardo Alt <leonardoaltt@gmail.com>
2016-11-23 11:34:57 +0100

    new options

--- solidifier.py
+++ solidifier.py
@@ -3,46 +3,78 @@
 """Solidifier.
 
 Usage:
-    solidifier.py verify [--filename=<solidity file>]
-    solidifier.py (-h | --help)
+    solidifier.py [verify | translate] [-o <outfile>] [-c <cfile>] [--verbose] <solfile> 
+    solidifier.py -h | --help | --version
 
 Options:
+    verify      Verify the solifity code.
+    translate   Translate the solidity code to C.
+
     -h --help     Show this screen.
     --version     Show version.
-    --file        Solidity filename
-
+    --verbose     Print the output from the model checker.
+    -o <outfile>  Specify output file (requires --verbose).
+    -c <cfile>    Specify output translated C file.
 """
+
+from docopt import docopt
 import subprocess, sys
-import docopt
+import os
 from tempfile import mkstemp
 
 import sol2c
 import utils
 
-from docopt import docopt
-
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='El solidificador')
-    fname = arguments['--filename']
+    fname = arguments['<solfile>']
+    outname = arguments['-o']
+    verbose = arguments['--verbose']
+    verify = arguments['verify']
+    translate = arguments['translate']
+    coutfile = arguments['-c']
 
-    solc_process = subprocess.Popen(['solc', '--ast-json', fname], stdout=subprocess.PIPE)
-    out, err = solc_process.communicate()
-    if err != None:
-        print 'Error running solc', err
-        sys.exit(1)
-    (_, tmp_json) = mkstemp()
+    if verify or translate:
+        solc_process = subprocess.Popen(['solc', '--ast-json', fname], stdout=subprocess.PIPE)
+        out, err = solc_process.communicate()
+        if err != None:
+            print 'Error running solc', err
+            sys.exit(1)
+        (_, tmp_json) = mkstemp()
 
-    with open(tmp_json, 'w') as f:
-        f.write(out)
+        with open(tmp_json, 'w') as f:
+            f.write(out)
     
-    utils.purify_json(tmp_json)
-    t = sol2c.CTranslator()
-    C_code = t.translate_to_C(tmp_json)
-    
-    with open('tmp.c', 'w') as f:
-        f.write(C_code)
+        utils.purify_json(tmp_json)
+        t = sol2c.CTranslator()
+        C_code = t.translate_to_C(tmp_json)
+        
+        if coutfile != None:
+            filename, fileext = os.path.splitext(coutfile)
+            if fileext != '.c':
+                print 'Output C file should have .c extension'
+                sys.exit(1)
+            cfile = coutfile
+            with open(coutfile, 'w') as f:
+                f.write(C_code)
+        elif translate:
+            print C_code
+                
+    if verify:
+        if coutfile == None:
+            (_, cfile) = mkstemp(suffix='.c', dir='.')
+            with open(cfile, 'w') as f:
+                f.write(C_code)
 
-    seahorn_process = subprocess.Popen(['sea', 'pf', 'tmp.c'], stdout=subprocess.PIPE)
-    out, err = solc_process.communicate()
-    print out
+        seahorn_process = subprocess.Popen(['sea', 'pf', cfile], stdout=subprocess.PIPE)
+        out, err = seahorn_process.communicate()
+        if verbose:
+            if outname != None:
+                with open(outname, 'w') as f:
+                    f.write(out)
+            else:
+                print out
+
+        if coutfile == None:
+            os.remove(cfile)
 
