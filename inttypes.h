81c8a7764f437f6eb63a6ae03996d55cf1831924
Fynn <enriquefynn@gmail.com>
2016-12-12 10:18:13 +0100

    all uint* int*

--- inttypes.h
+++ inttypes.h
@@ -1,4 +1,78 @@
+#pragma once
+#include "stdint.h"
+
 typedef int bool;
-typedef int uint1;
+
+//UINT
+typedef int uint;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef int uint24;
+typedef uint32_t uint32;
+typedef int uint40;
+typedef int uint48;
+typedef int uint56;
+typedef uint64_t uint64;
+typedef int uint72;
+typedef int uint80;
+typedef int uint88;
+typedef int uint96;
+typedef int uint104;
+typedef int uint112;
+typedef int uint120;
+typedef int uint128;
+typedef int uint136;
+typedef int uint144;
+typedef int uint152;
+typedef int uint160;
+typedef int uint168;
+typedef int uint176;
+typedef int uint184;
+typedef int uint192;
+typedef int uint200;
+typedef int uint208;
+typedef int uint216;
+typedef int uint224;
+typedef int uint232;
+typedef int uint240;
+typedef int uint248;
 typedef int uint256;
+
+//INT
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int int24;
+typedef int32_t int32;
+typedef int int40;
+typedef int int48;
+typedef int int56;
+typedef int64_t int64;
+typedef int int72;
+typedef int int80;
+typedef int int88;
+typedef int int96;
+typedef int int104;
+typedef int int112;
+typedef int int120;
+typedef int int128;
+typedef int int136;
+typedef int int144;
+typedef int int152;
+typedef int int160;
+typedef int int168;
+typedef int int176;
+typedef int int184;
+typedef int int192;
+typedef int int200;
+typedef int int208;
+typedef int int216;
+typedef int int224;
+typedef int int232;
+typedef int int240;
+typedef int int248;
+typedef int int256;
+
+
+
 typedef int bytes32;
+
